e0a6ed3d7ad1040b8fdf4bafc1a285a901fc88cc
Kirill Yudin <kirillbatusai@gmail.com>
2019-03-20 11:05:25 +1000

    Add game logic

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-import greetUserByName from '..';
+import { checkUserAnswer, greetUserByName } from '../index';
 
 console.log('Welcome to the Brain Games!');
 
+console.log('Answer \'yes\' if number is even other wise \'no\'');
+
 greetUserByName();
+checkUserAnswer();
